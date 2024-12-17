be46937bbbd5107ce9bb9a508d753e3e632df30a
Clevrem <clevrem2@gmail.com>
2024-12-17 16:16:29 +0500

    3)Categories degan class ichiga Scaffold berdim

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart'; // bu yerda men material paketini yuklab oldim
 
-void main() => runApp(const RecipeApp());// bu yerda recipe appni ishga tushurish tugmasi
+void main() =>
+    runApp(const RecipeApp()); // bu yerda recipe appni ishga tushurish tugmasi
 
-class RecipeApp extends StatelessWidget {//recipe appni class yaratdik!
+class RecipeApp extends StatelessWidget {  //recipe appni class yaratdik!
+
   const RecipeApp({super.key});
 
   @override
-  Widget build(BuildContext context) {  //
-    return MaterialApp(
+  Widget build(BuildContext context) {
+    //
+    return  const MaterialApp( // Materila app packagdan keladi doim dasturda bir marotaba ishlatiladi!
       home: CategoriesPage(),
+    );
+  }
+}
+
+class CategoriesPage extends StatelessWidget {  // return uchun Categories pagega alohida class olib o'tdim!
+  const CategoriesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
     );
   }
 }
