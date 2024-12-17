e7e18421a89f2514ced2dbb0779b95bc4b816064
Clevrem <clevrem2@gmail.com>
2024-12-17 16:48:30 +0500

    Tayyor!

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,131 @@
-import 'package:flutter/material.dart'; // bu yerda men material paketini yuklab oldim
+import 'package:flutter/material.dart';
 
-void main() =>
-    runApp(const RecipeApp()); // bu yerda recipe appni ishga tushurish tugmasi
 
-class RecipeApp extends StatelessWidget {  //recipe appni class yaratdik!
+void main() => runApp(const RecipeApp());
 
+class RecipeApp extends StatelessWidget {
   const RecipeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //
-    return  const MaterialApp( // Materila app packagdan keladi doim dasturda bir marotaba ishlatiladi!
+    return const MaterialApp(
       home: CategoriesPage(),
     );
   }
 }
 
-class CategoriesPage extends StatelessWidget {  // return uchun Categories pagega alohida class olib o'tdim!
+class CategoriesPage extends StatelessWidget {
   const CategoriesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: Colors.orangeAccent, // dastur orqa oynasiga rang qo'yish
+      backgroundColor: Color(0xFF1C0F0D),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MainCategoryItem(image: "assets/seafood.png", text: "Seafood"),
+          SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CategoryItem(image: "assets/lunch.png", text: "Lunch"),
+              CategoryItem(image: "assets/breakfast.png", text: "Breakfast"),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CategoryItem(image: "assets/dinner.png", text: "Dinner"),
+              CategoryItem(image: "assets/vegan.png", text: "Vegan"),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CategoryItem(image: "assets/dessert.png", text: "Dessert"),
+              CategoryItem(
+                image: "assets/drinks.png",
+                text: "Drinks",
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
 
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({super.key, required this.image, required this.text});
+
+  final String image, text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(
+          child: SizedBox(
+            width: 159,
+            height: 145,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(13),
+              child: Image(
+                image: AssetImage(image),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 6),
+        Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            decoration: TextDecoration.none,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MainCategoryItem extends StatelessWidget {
+  const MainCategoryItem({super.key, required this.image, required this.text});
+
+  final String image, text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            decoration: TextDecoration.none,
+          ),
+        ),
+        const SizedBox(height: 6),
+        Center(
+          child: SizedBox(
+            width: 356,
+            height: 149,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(13),
+              child: Image(
+                image: AssetImage(image),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
